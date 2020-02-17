648afef3551b874914539e75668bab254ebcef88
feio-ai <feioaccel@gmail.com>
2020-02-17 13:54:08 -0600

    random number update

--- app/Main.hs
+++ app/Main.hs
@@ -12,22 +12,53 @@ import Control.Monad.ST
 
 main :: IO ()
 main = do
+    putStrLn "How Many Iterations?"
     iter <- getLine
-    let ind = convToInt iter
-    let oldVec = createVecIO ind
+    putStrLn "Stock Price:"
+    so <- getLine
+    putStrLn "Implied Volatility"
+    sig <- getLine
+    putStrLn "Rate"
+    r <- getLine
+    let iter' = convToInt iter
+    let oldVec = createVecIO iter'
     the_nums <- sequence oldVec -- [IO Float] -> IO [Float]
     putStrLn (show the_nums)
+    print the_nums
 
 
-createVecIO :: Int -> [IO Float]
+createVecIO :: Float -> [IO Float]
 createVecIO ind 
     | ind <= 0      = []
     | otherwise     = do
         let va = withSystemRandom $ \(gen::GenST s) -> uniform gen :: ST s (Float)
         va : createVecIO (ind-1)
 
-convToInt :: [Char] -> Int
-convToInt input = read input :: Int
+convToInt :: [Char] -> Float
+convToInt input = read input :: Float
+
+------------------------------------------------------------------------------------------------
+-- Monte given a time step
+-- montePrice' :: [Float] -> Float -> Float -> 
+
+montePrice :: Double -> Double -> Double -> Double -> Double -> [Double]
+montePrice so sig r eps dt = Prelude.map f times
+    where
+        times = timeStep dt
+        f x = so * (exp (y + z))
+          where
+              y = (r - ((sig**2) / 2)) * x
+              z = (sig * sqrt x) * eps
+timeStep :: Double -> [Double]
+timeStep dt 
+    | dt >= 1     = []
+    | otherwise   = [0, dt .. 1]
+
+timeStep' :: Float -> [Float]
+timeStep' dt 
+    | dt >= 1     = []
+    | otherwise   = [0, dt .. 1]
+
 
 ---------------------------------------------------------------------------------------
 -- Vec Utils
@@ -46,20 +77,3 @@ convertVec = DV.map f
     where
         f x = fromIntegral $ abs x
 
-
-------------------------------------------------------------------------------------------------
--- Monte given a time step
-montePrice :: Double -> Double -> Double -> Double -> Double -> [Double]
-montePrice so sig r eps dt = Prelude.map f times
-    where
-        times = timeStep dt
-        f x = so * (exp (y + z))
-          where
-              y = (r - ((sig**2) / 2)) * x
-              z = (sig * sqrt x) * eps
-timeStep :: Double -> [Double]
-timeStep dt 
-    | dt >= 1     = []
-    | otherwise   = [0, dt .. 1]
-
----------------------------------------------------------------------------------------------------
